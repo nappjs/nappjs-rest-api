745d73411206d512944aa5905553af821dd72b73
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-11-27 05:25:26 +0100

    Update api path envvar to REST_API_PATH

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const Router = require("express").Router;
 const CoreDataRest = require("js-core-data-rest");
 const bodyParser = require("body-parser");
 
-const REST_API_URL = process.env.REST_API_URL || "/";
+const REST_API_PATH = process.env.REST_API_URL || "/";
 
 module.exports = database => {
   let app = new Router();
 
-  app.use(REST_API_URL, bodyParser.json());
-  app.use(REST_API_URL, CoreDataRest.rest(database));
+  app.use(REST_API_PATH, bodyParser.json());
+  app.use(REST_API_PATH, CoreDataRest.rest(database));
 
   return app;
 };
